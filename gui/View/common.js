8cf05a5deb47cf5ac0a5b4a6052c6c5f66fd36e3
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-02 01:42:02 +0900

    ➕ 빈 필드 오류 표시 함수 추가

--- gui/View/common.js
+++ gui/View/common.js
@@ -1,19 +1,31 @@
-module.exports.Field_getValue = function (field) {
-    return field.firstElementChild.lastElementChild.innerHTML;
-};
+function Field_addTransition(field) {
+    field.firstElementChild.classList.add('transition');
+}
+module.exports.Field_addTransition = Field_addTransition;
 
-module.exports.Field_preventMultiline = function (field) {
-    field.firstElementChild.lastElementChild.addEventListener('keydown',
+function Field_getValueDom(field) {
+    return field.firstElementChild.lastElementChild;
+}
+module.exports.Field_getValueDom = Field_getValueDom;
+
+function Field_getValue(field) {
+    return Field_getValueDom(field).innerHTML;
+}
+module.exports.Field_getValue = Field_getValue;
+
+function Field_preventMultiline(field) {
+    Field_getValueDom(field).addEventListener('keydown',
         (event) => {
             if (event.keyCode === 13) {
                 event.preventDefault();
             }
         }
     );
-};
+}
+module.exports.Field_preventMultiline = Field_preventMultiline;
 
-module.exports.Field_invokeWithEnter = function (field, func) {
-    field.firstElementChild.lastElementChild.addEventListener('keydown',
+function Field_invokeWithEnter(field, func) {
+    Field_getValueDom(field).addEventListener('keydown',
         (event) => {
             if (event.keyCode === 13) {
                 func();
@@ -22,3 +34,14 @@ module.exports.Field_invokeWithEnter = function (field, func) {
         }
     );
 }
+module.exports.Field_invokeWithEnter = Field_invokeWithEnter;
+
+function Field_setInvalid(field) {
+    field.classList.add('value-error');
+    Field_getValueDom(field).addEventListener('input',
+        (event) => {
+            field.classList.remove('value-error');
+        }
+    );
+}
+module.exports.Field_setInvalid = Field_setInvalid;
