cad496a9dccf8f679781b67792c3f9a07528dd8a
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-02 18:05:42 +0900

    ➕ 이메일 내용 처리 추가

--- gui/View/detail.js
+++ gui/View/detail.js
@@ -1,3 +1,10 @@
+const { ipcRenderer } = require('electron');
+const common = require('./common.js');
+
+let emailTitle_dom = null;
+let emailSubtitle_dom = null;
+let emailBody_dom = null;
+
 function getDataId() {
     const prefix = '---data-id=';
     for (let i in window.process.argv) {
@@ -8,8 +15,25 @@ function getDataId() {
     return 'null';
 }
 
-let titleBar;
-window.onload = function() {
-    titleBar = document.getElementById('title-bar');
-    titleBar.innerHTML = getDataId();
+function refreshDetail() {
+    common.setLoading();
+    ipcRenderer.send('detail-refresh', {
+        dataId: getDataId()
+    });
 }
+
+window.onload = function() {
+    emailTitle_dom = document.getElementById('email-title');
+    emailSubtitle_dom = document.getElementById('email-subtitle');
+    emailBody_dom = document.getElementById('email-body');
+    common.setTitle('Dear My Professor');
+    refreshDetail();
+}
+
+ipcRenderer.on('detail-refresh-reply', (event, arg) => {
+    common.unsetLoading();
+    common.setTitle('Dear My Professor - ' + arg.title);
+    emailTitle_dom.innerText = arg.title;
+    emailSubtitle_dom.innerText = arg.subtitle;
+    emailBody_dom.innerText = arg.body;
+});
