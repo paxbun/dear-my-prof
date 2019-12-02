6175e7346293788ad121fe09c1c8cddca2afe73b
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-02 17:40:43 +0900

    ➕ 버튼 이벤트 처리 추가

--- gui/View/root.js
+++ gui/View/root.js
@@ -8,6 +8,14 @@ function refreshMailList() {
     ipcRenderer.send('mail-refresh');
 }
 
+function openNewEmail() {
+    ipcRenderer.send('create-new-email-window');
+}
+
+function openTemplates() {
+    ipcRenderer.send('create-templates-window');
+}
+
 window.onload = function () {
     mailList_dom = document.getElementById('mail-list');
     common.setTitle('Dear My Professor - 메일함');
@@ -16,9 +24,13 @@ window.onload = function () {
 
 ipcRenderer.on('mail-refresh-reply', (event, arg) => {
     for (let i in arg) {
-        mailList_dom.appendChild(
-            common.ItemThree_new(arg[i].title, arg[i].subtitle, arg[i].body)
-        );
+        let item = common.ItemThree_new(arg[i].title, arg[i].subtitle, arg[i].body);
+        item.addEventListener('click', (event) => {
+            ipcRenderer.send('create-detail-window',  {
+                dataId: arg[i].dataId
+            });
+        });
+        mailList_dom.appendChild(item);
     }
 
     common.unsetLoading();
