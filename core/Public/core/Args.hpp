3919061b45b7bf14a1449f80c84cf84214dcac03
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 07:43:13 +0900

    ➕ nlohmann_json 추가

--- core/Public/core/Args.hpp
+++ core/Public/core/Args.hpp
@@ -4,11 +4,15 @@
 #ifndef __H_CORE_ARGS__
 #define __H_CORE_ARGS__
 
+#include <nlohmann/json.hpp>
+
 /*
     Args 클래스는 각 모듈들이 서로 통신할 때 추가로 전달되는 인자들을 보관하기
     위해 사용됩니다.
 */
-class Args
-{};
+using Args = nlohmann::json;
+
+// class Args
+//{};
 
 #endif
