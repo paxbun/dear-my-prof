b2db9b868f691086149303f335bbb533321873f1
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 02:13:12 +0900

    ➕ Args 클래스 추가

---
+++ core/Public/core/Args.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#ifndef __H_CORE_ARGS__
+#define __H_CORE_ARGS__
+
+/*
+    Args 클래스는 각 모듈들이 서로 통신할 때 추가로 전달되는 인자들을 보관하기
+    위해 사용됩니다.
+*/
+class Args
+{};
+
+#endif
