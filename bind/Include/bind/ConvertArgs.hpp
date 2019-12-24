ec71d3024a6b110abcaa2cbd88c57de2ab247237
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 04:36:20 +0900

    ➕ ConvertArgs 함수 추가

---
+++ bind/Include/bind/ConvertArgs.hpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#include <core/Args.hpp>
+
+#include <napi.h>
+
+Napi::Object ConvertArgs(Args const& args) {}
---
+++ bind/Source/ConvertArgs.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#include <bind/ConvertArgs.hpp>
+
+Napi::Object ConvertArgs(Args const& args) {}
