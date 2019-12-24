15054a9acbd67129bfe5ad5c6f084aeeebaf261f
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 01:18:26 +0900

    ➕ HasApp 클래스 추가

---
+++ core/Public/core/HasApp.hpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#ifndef __H_CORE_HAS_APP__
+#define __H_CORE_HAS_APP__
+
+/*
+    HasApp 클래스는 App을 멤버 변수로 가지고, getter를 제공하는 클래스입니다.
+    View, Presenter 등 App 객체에 접근해야 하는 클래스는 이 클래스를
+    상속하여야합니다.
+*/
+class HasApp
+{
+    /*
+        App 클래스는 HasApp 클래스의 App 멤버 변수에 접근할 수 있습니다.
+    */
+    friend class App;
+
+  private:
+    App* _app;
+
+  public:
+    /*
+        현재 애플리케이션의 App 객체를 반환합니다.
+    */
+    App* app()
+    {
+        return _app;
+    }
+
+  public:
+    HasApp() : _app(nullptr) {}
+};
+
+#endif
