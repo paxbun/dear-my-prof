7990eb280b1d3007f8986834b4eff43cf3ddcc8b
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 02:36:11 +0900

    ✏ 삭제된 테스트 파일을 참조하는 함수 수정

--- bind/Source/Bind.cpp
+++ bind/Source/Bind.cpp
@@ -1,13 +1,11 @@
 #include <bind/Bind.hpp>
-#include <core/Core.hpp>
 
 Napi::Object InitAll(Napi::Env env, Napi::Object exports)
 {
     exports.Set("hello",
                 Napi::Function::New(
                     env, [](const Napi::CallbackInfo& info) -> Napi::String {
-                        return Napi::String::New(
-                            info.Env(), CoreFooFunction("Hello, world!"));
+                        return Napi::String::New(info.Env(), "Hello, world!");
                     }));
     return exports;
 }
