fc3a47a31c6a810daed7ba784a33b6c337f18b91
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 16:43:03 +0900

    ➕ 테스트 코드 추가

--- core/Source/DetailPresenter.cpp
+++ core/Source/DetailPresenter.cpp
@@ -2,5 +2,13 @@
 // Author: paxbun
 
 #include <core/DetailPresenter.hpp>
+#include <core/View.hpp>
 
-void DetailPresenter::Input(std::string const& event_name, Args const& args) {}
+void DetailPresenter::Input(std::string const& event_name, Args const& args)
+{
+    // TODO
+    view()->Output("detail-refresh-reply",
+                   { { "title", u8"제목" },
+                     { "subtitle", u8"부제목" },
+                     { "body", u8"내용" } });
+}
