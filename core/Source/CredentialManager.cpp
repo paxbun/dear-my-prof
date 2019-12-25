d6900172cae6ba06e652892c8397b1f1ff700899
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 16:29:01 +0900

    ➕ 테스트 코드 추가

--- core/Source/CredentialManager.cpp
+++ core/Source/CredentialManager.cpp
@@ -10,5 +10,13 @@ bool CredentialManager::TryLogin(std::string const& id,
                                  std::string const& sid,
                                  std::string const& name) noexcept
 {
-    return !(id.empty() || pw.empty() || sid.empty() || name.empty());
+    if (!(id.empty() || pw.empty() || sid.empty() || name.empty()))
+    {
+        _id   = id;
+        _pw   = pw;
+        _sid  = sid;
+        _name = name;
+    }
+    else
+        return false;
 }
