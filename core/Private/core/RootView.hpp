d8047d91d5a883993eb2fbe03b960e5c21f78cf0
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 16:49:03 +0900

    ✨ 필요한 바인딩 모두 등록

--- core/Private/core/RootView.hpp
+++ core/Private/core/RootView.hpp
@@ -4,12 +4,28 @@
 #ifndef __H_CORE_ROOT_VIEW__
 #define __H_CORE_ROOT_VIEW__
 
+#include <core/EmailListPresenter.hpp>
+#include <core/OpenDetailPresenter.hpp>
+#include <core/OpenNewEmailPresenter.hpp>
+#include <core/OpenTemplatePresenter.hpp>
 #include <core/View.hpp>
 
 class RootView : public View
 {
+  private:
+    OpenTemplateListPresetner _openTemplateList;
+    OpenNewEmailPresenter     _openNewEmail;
+    EmailListPresenter        _emailList;
+    OpenDetailPresenter       _openDetail;
+
   public:
-    RootView() : View("root.html", {}) {}
+    RootView()
+        : View("root.html",
+               { { "create-templates-window", &_openTemplateList },
+                 { "create-new-email-window", &_openNewEmail },
+                 { "mail-refresh", &_emailList },
+                 { "create-detail-window", &_openDetail } })
+    {}
 };
 
 #endif
