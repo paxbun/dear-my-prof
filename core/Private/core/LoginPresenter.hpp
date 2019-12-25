85f175fac61e3d1a2d34cf548b510f666f7cb457
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 14:41:43 +0900

    ➕ 아이디 비밀번호 확인 추가

--- core/Private/core/LoginPresenter.hpp
+++ core/Private/core/LoginPresenter.hpp
@@ -5,6 +5,7 @@
 #define __H_CORE_LOGIN_PRESENTER__
 
 #include <core/Presenter.hpp>
+#include <core/RootView.hpp>
 #include <core/View.hpp>
 
 class LoginPresenter : public Presenter
@@ -12,7 +13,20 @@ class LoginPresenter : public Presenter
   public:
     virtual void Input(std::string const& event_name, Args const& args) override
     {
-        view()->Output("login-failed");
+        auto id_it = args.find("id");
+        auto pw_it = args.find("pw");
+        if (id_it != args.end() && pw_it != args.end())
+        {
+            auto id = id_it.value().get<std::string>();
+            auto pw = pw_it.value().get<std::string>();
+
+            if (id == pw)
+                app()->Navigate(view(), new RootView);
+            else
+                view()->Output("login-failed");
+        }
+        else
+            view()->Output("login-failed");
     }
 };
 
