f46b16c6983fbb61e0874f4d8cbce0e75bc0e899
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 13:14:28 +0900

    ➕ 한글 체크 추가

--- core/Source/CodaStringTransform.cpp
+++ core/Source/CodaStringTransform.cpp
@@ -1,12 +1,15 @@
 // Copyright (c) 2019 Dear My Professor Authors
 // Author: paxbun
 
-#include <core/StringTransform.hpp>
+#include <core/CodaStringTransform.hpp>
 
 #include <boost/locale.hpp>
 
 bool CodaStringTransform::_EndsWithCoda(std::string const& input)
 {
-    auto conv = boost::locale::utf_to_utf<wchar_t>(input);
-    return (conv.back() - 0xAC00) % 28;
+    auto conv = boost::locale::conv::utf_to_utf<wchar_t>(input);
+    auto back = conv.back();
+
+    return conv.back() >= 0xAC00 && conv.back() <= 0xD7A3
+           && ((conv.back() - 0xAC00) % 28 != 0);
 }
