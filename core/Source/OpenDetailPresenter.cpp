d12f82f4904c69ad9d3bfc467cf53ea32a36685a
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 19:44:54 +0900

    🔨 알맞은 서식의 생성인자로 수정

--- core/Source/OpenDetailPresenter.cpp
+++ core/Source/OpenDetailPresenter.cpp
@@ -3,11 +3,16 @@
 
 #include <core/App.hpp>
 #include <core/DetailView.hpp>
+#include <core/Email.hpp>
 #include <core/HasApp.hpp>
 #include <core/OpenDetailPresenter.hpp>
 #include <core/View.hpp>
 
 void OpenDetailPresenter::Input(std::string const& event_name, Args const& args)
 {
-    app()->NewWindow(new DetailView(Args(args)), view());
+    Args new_args = Args::array();
+    new_args.push_back(std::string("---data-id=")
+                       + args.at("id").get<std::string>());
+
+    app()->NewWindow(new DetailView(std::move(new_args)), view());
 }
