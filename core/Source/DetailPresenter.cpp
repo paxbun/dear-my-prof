dbf46daeac915f5173747785e78204bc2a53267a
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 16:41:15 +0900

    ➕ DeatilPresenter 클래스 추가

---
+++ core/Source/DetailPresenter.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#include <core/DetailPresenter.hpp>
+
+void DetailPresenter::Input(std::string const& event_name, Args const& args) {}
