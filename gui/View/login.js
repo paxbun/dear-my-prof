fba63dba7df1d5aacfc5e71b10c8c75aa75be44a
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 14:52:15 +0900

    ➕ 이름/학번 입력란 추가

--- gui/View/login.js
+++ gui/View/login.js
@@ -3,23 +3,35 @@ const common = require('./common.js');
 
 let loginId_dom = null;
 let loginPw_dom = null;
+let loginName_dom = null;
+let loginSid_dom = null;
 
 function login() {
     let id = common.Field_getValue(loginId_dom);
     let pw = common.Field_getValue(loginPw_dom);
+    let name = common.Field_getValue(loginName_dom);
+    let sid = common.Field_getValue(loginSid_dom);
 
-    if (!id || !pw) {
+    if (!id || !pw || !name || !sid) {
         if (!id) {
             common.Field_setInvalid(loginId_dom);
         }
         if (!pw) {
             common.Field_setInvalid(loginPw_dom);
         }
+        if (!name) {
+            common.Field_setInvalid(loginName_dom);
+        }
+        if (!sid) {
+            common.Field_setInvalid(loginSid_dom);
+        }
     } else {
         common.setLoading();
         ipcRenderer.send('login', {
             id: common.Field_getValue(loginId_dom),
-            pw: common.Field_getValue(loginPw_dom)
+            pw: common.Field_getValue(loginPw_dom),
+            name: common.Field_getValue(loginName_dom),
+            sid: common.Field_getValue(loginSid_dom)
         });
     }
 }
@@ -27,12 +39,18 @@ function login() {
 window.onload = function() {
     loginId_dom = document.getElementById('login-id');
     loginPw_dom = document.getElementById('login-pw');
+    loginName_dom = document.getElementById('login-name');
+    loginSid_dom = document.getElementById('login-sid');
 
     common.Field_addTransition(loginId_dom);
     common.Field_addTransition(loginPw_dom);
+    common.Field_addTransition(loginName_dom);
+    common.Field_addTransition(loginSid_dom);
 
     common.Field_invokeWithEnter(loginId_dom, login);
     common.Field_invokeWithEnter(loginPw_dom, login);
+    common.Field_invokeWithEnter(loginName_dom, login);
+    common.Field_invokeWithEnter(loginSid_dom, login);
 
     common.setTitle('Dear My Professor - 로그인');
 }
@@ -40,5 +58,7 @@ window.onload = function() {
 ipcRenderer.on('login-failed', (event, arg) => {
     common.Field_setInvalid(loginId_dom);
     common.Field_setInvalid(loginPw_dom);
+    common.Field_setInvalid(loginName_dom);
+    common.Field_setInvalid(loginSid_dom);
     common.unsetLoading();
 });
