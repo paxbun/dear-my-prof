efc7c38a32b702e9461365418fffa13ded526e5a
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 15:14:33 +0900

    ➕ 주석 추가

--- core/Private/core/CredentialManager.hpp
+++ core/Private/core/CredentialManager.hpp
@@ -48,10 +48,21 @@ class CredentialManager
     CredentialManager() {}
 
   public:
+    /*
+        로그인을 시도하고, 성공시 로그인 정보를 저장합니다.
+        Parameters
+        id: 아이디입니다.
+        pw: 비밀번호입니다.
+        sid: 학번입니다.
+        name: 이름입니다.
+
+        Return Value
+        성공시 true를 반환하고, 아닐 경우 false를 반환합니다.
+    */
     bool TryLogin(std::string const& id,
                   std::string const& pw,
                   std::string const& sid,
-                  std::string const& name);
+                  std::string const& name) noexcept;
 };
 
 #endif
