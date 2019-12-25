b8249636e129a0c690fe7b1dcccebdd3b7569b66
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 21:21:57 +0900

    🔨 생성자 충돌 수정

--- core/Private/core/Address.hpp
+++ core/Private/core/Address.hpp
@@ -5,6 +5,7 @@
 #define __H_CORE_ADDRESS__
 
 #include <string>
+#include <type_traits>
 
 /*
     Address 클래스는 이메일 주소를 담기 위한 자료형입니다. 이메일을 보낼 때
@@ -17,7 +18,13 @@ class Address
     std::string realName;
 
   public:
-    template <typename EmailT = std::string, typename RealNameT = std::string>
+    template <
+        typename EmailT       = std::string,
+        typename RealNameT    = std::string,
+        std::enable_if_t<!std::is_same_v<std::remove_const_t<
+                                             std::remove_reference_t<EmailT>>,
+                                         Address>,
+                         int> = 0>
     Address(EmailT&& email, RealNameT&& realName = RealNameT())
         : email(std::forward<EmailT>(email)),
           realName(std::forward<RealNameT>(realName))
