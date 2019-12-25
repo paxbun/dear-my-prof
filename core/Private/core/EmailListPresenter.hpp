48b39ce91b9c2a7b234c98832f38e45e79d9181c
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 16:53:00 +0900

    ➕ EmailListPresenter 클래스 추가

---
+++ core/Private/core/EmailListPresenter.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#ifndef __H_CORE_EMAIL_LIST_PRESENTER__
+#define __H_CORE_EMAIL_LIST_PRESENTER__
+
+#include <core/Presenter.hpp>
+
+class EmailListPresenter : public Presenter
+{
+  public:
+    virtual void Input(std::string const& event_name,
+                       Args const&        args) override;
+};
+
+#endif
---
+++ core/Source/EmailListPresenter.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#include <core/EmailListPresenter.hpp>
+#include <core/View.hpp>
+
+void EmailListPresenter::Input(std::string const& event_name, Args const& args)
+{
+    // TODO
+    Args args = Args::array();
+    for (int i = 0; i < 5; ++i)
+        args.push_back({
+            { "title", u8"제목" },
+            { "subtitle", u8"부제목" },
+            { "body", u8"내용" },
+            { "id", 12345 },
+        });
+    view()->Output("mail-refresh-reply", args);
+}
