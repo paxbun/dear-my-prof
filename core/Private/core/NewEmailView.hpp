69c2d5bd573009e88f5d74fcd8e8835f7fb487fd
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 16:58:18 +0900

    ➕ NewEmailView 클래스 추가

---
+++ core/Private/core/NewEmailView.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#ifndef __H_CORE_NEW_EMAIL_VIEW__
+#define __H_CORE_NEW_EMAIL_VIEW__
+
+#include <core/Args.hpp>
+#include <core/View.hpp>
+
+class NewEmailView : public View
+{
+    // TODO
+
+  public:
+    NewEmailView() : View("newemail.html", {}) {}
+};
+
+#endif
