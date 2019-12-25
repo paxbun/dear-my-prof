c24bd8a4f0f9716860334353a03ec086555e4227
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-25 17:00:57 +0900

    ➕ TemplateListView 클래스 추가

---
+++ core/Private/core/TemplateListView.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#ifndef __H_CORE_TEMPLATE_LIST_VIEW__
+#define __H_CORE_TEMPLATE_LIST_VIEW__
+
+#include <core/Args.hpp>
+#include <core/View.hpp>
+
+class TemplateListView : public View
+{
+    // TODO
+
+  public:
+    TemplateListView() : View("template.html", {}) {}
+};
+
+#endif
