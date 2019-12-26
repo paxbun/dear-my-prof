23fd13b4f5c73bcac0a72245c5fee3cf0610319a
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-26 13:30:05 +0900

    🔨 임시 변수 사용하도록 수정

--- core/Source/CodaStringTransform.cpp
+++ core/Source/CodaStringTransform.cpp
@@ -10,6 +10,5 @@ bool CodaStringTransform::_EndsWithCoda(std::string const& input)
     auto conv = boost::locale::conv::utf_to_utf<wchar_t>(input);
     auto back = conv.back();
 
-    return conv.back() >= 0xAC00 && conv.back() <= 0xD7A3
-           && ((conv.back() - 0xAC00) % 28 != 0);
+    return back >= 0xAC00 && back <= 0xD7A3 && ((back - 0xAC00) % 28 != 0);
 }
