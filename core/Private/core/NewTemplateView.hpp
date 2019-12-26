b0bd1ee1764ec90f73d10b2f11df2fe259de8c24
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-26 10:48:13 +0900

    ➕ NewTemplateView 클래스 추가

---
+++ core/Private/core/NewTemplateView.hpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#ifndef __H_CORE_NEW_TEMPLATE_VIEW__
+#define __H_CORE_NEW_TEMPLATE_VIEW__
+
+#include <core/Args.hpp>
+#include <core/View.hpp>
+
+class NewTemplateView : public View
+{
+    // TODO
+
+  public:
+    NewTemplateView() : View("newtemplate.html", {}) {}
+};
+
+#endif
