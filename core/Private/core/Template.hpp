9dfa60ffd75c5dd4a7e05370e1571d5a0d24d61a
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-26 09:39:34 +0900

    ➕ Template 클래스 추가

---
+++ core/Private/core/Template.hpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#ifndef __H_CORE_TEMPLATE__
+#define __H_CORE_TEMPLATE__
+
+#include <core/Email.hpp>
+#include <core/TemplateString.hpp>
+
+#include <iostream>
+#include <map>
+#include <set>
+
+/*
+    Template 클래스는 하나의 템플릿을 저장하는 모델입니다.
+*/
+class Template
+{
+  public:
+    static Template ParseFrom(std::istream& is);
+
+  private:
+    TemplateString _subject;
+    TemplateString _content;
+
+  public:
+    template <typename SubjectT = TemplateString,
+              typename ContentT = TemplateString>
+    Template(SubjectT&& subject, ContentT&& content)
+        : _subject(std::forward<SubjectT>(subject)),
+          _content(std::forward<ContentT>(content))
+    {}
+
+  public:
+    /*
+        Parameters
+        map: 매개변수의 이름과 들어갈 값을 대응시킨 맵입니다.
+
+        Return Value
+        주어진 맵에 따라 변수를 집어넣은 결과를 반환합니다.
+    */
+    Template Generate(std::unordered_map<std::string, std::string> const& map);
+
+    /*
+        Return Value
+        이 템플릿을 Email 객체로 변환합니다. 채워지지 않은 매개변수는 ${}의
+        형태로 나타납니다.
+    */
+    Email ToEmail();
+
+    /*
+        Return Value
+        이 템플릿의 모든 매개변수 이름의 목록을 반환합니다.
+    */
+    std::set<std::string> GetAllParameters();
+};
+
+#endif
---
+++ core/Source/Template.cpp
@@ -0,0 +1,54 @@
+// Copyright (c) 2019 Dear My Professor Authors
+// Author: paxbun
+
+#include <core/Template.hpp>
+
+#include <string>
+
+Template Template::ParseFrom(std::istream& is)
+{
+    std::string line;
+    do
+    {
+        std::getline(is, line);
+    } while (line != "BEGIN");
+
+    // Read subject
+    std::getline(is, line);
+    auto subject = TemplateString::ParseFrom(line);
+
+    // Read content
+    std::string content;
+    while (true)
+    {
+        std::getline(is, line);
+        if (line == "END")
+            break;
+        content.append(line);
+        content.push_back('\n');
+    }
+    auto content = TemplateString::ParseFrom(content);
+
+    return Template(std::move(subject), std::move(content));
+}
+Template
+Template::Generate(std::unordered_map<std::string, std::string> const& map)
+{
+    return Template(_subject.Generate(map), _content.Generate(map));
+}
+
+Email Template::ToEmail()
+{
+    return Email(
+        _subject.ToString(), Address(), Address(), _content.ToString());
+}
+
+std::set<std::string> Template::GetAllParameters()
+{
+    std::set<std::string> rtn;
+
+    for (auto const& param : _subject.param()) rtn.insert(param.name);
+    for (auto const& param : _content.param()) rtn.insert(param.name);
+
+    return rtn;
+}
