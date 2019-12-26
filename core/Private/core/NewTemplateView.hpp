78e5df68b40532bab97c5ef86f7e4d76fbf35814
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-26 13:06:15 +0900

    ✨ 프레젠터 바인딩 완성

--- core/Private/core/NewTemplateView.hpp
+++ core/Private/core/NewTemplateView.hpp
@@ -5,14 +5,18 @@
 #define __H_CORE_NEW_TEMPLATE_VIEW__
 
 #include <core/Args.hpp>
+#include <core/SaveTemplatePresenter.hpp>
 #include <core/View.hpp>
 
 class NewTemplateView : public View
 {
-    // TODO
+  private:
+    SaveTemplatePresenter _saveTemplate;
 
   public:
-    NewTemplateView() : View("newtemplate.html", {}) {}
+    NewTemplateView()
+        : View("newtemplate.html", { { "save-template", &_saveTemplate } })
+    {}
 };
 
 #endif
