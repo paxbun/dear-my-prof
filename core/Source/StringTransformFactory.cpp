d194eff09791d44b9079965488fc408e83167144
Chanjung Kim <freiyer.paxbun@gmail.com>
2019-12-26 09:19:18 +0900

    ➕ 목적어 문자열 변환함수 추가

--- core/Source/StringTransformFactory.cpp
+++ core/Source/StringTransformFactory.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2019 Dear My Professor Authors
 // Author: paxbun
 
+#include <core/ObjectStringTransform.hpp>
 #include <core/StringTransformFactory.hpp>
 #include <core/SubjectStringTransform.hpp>
 #include <core/ThemeStringTransform.hpp>
@@ -11,14 +12,19 @@ namespace
 StringTransform        _default;
 ThemeStringTransform   _theme;
 SubjectStringTransform _subject;
+ObjectStringTransform  _object;
 
 } // namespace
 
 std::unordered_map<std::string, StringTransform*>
-    StringTransformFactory::_transformMap = { { { u8"은", &_theme },
-                                                { u8"는", &_theme },
-                                                { u8"이", &_subject },
-                                                { u8"가", &_subject } } };
+    StringTransformFactory::_transformMap = { {
+        { u8"은", &_theme },
+        { u8"는", &_theme },
+        { u8"이", &_subject },
+        { u8"가", &_subject },
+        { u8"을", &_object },
+        { u8"를", &_object },
+    } };
 
 StringTransform*
 StringTransformFactory::GetTransform(std::string const& transform_name)
